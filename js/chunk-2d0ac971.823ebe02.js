3d599f4ae331badd00aa601b71625581ae03b3cf
S3R <git@oop.ir>
2023-05-16 08:21:52 +0330

    first commit

---
+++ js/chunk-2d0ac971.823ebe02.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ac971"],{"19d7":function(n,u,c){"use strict";c.r(u);var t=function(){var n=this,u=n._self._c;return u("div",[n._v(" hi ")])},e=[],i={},l=i,s=c("2877"),a=Object(s["a"])(l,t,e,!1,null,null,null);u["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0ac971.823ebe02.js.map
